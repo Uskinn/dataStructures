f210e96338c632137e61abe29c66de394b9e4783
Uskinn <s.nevzor@yahoo.com>
2017-01-11 12:46:57 -0500

    savind process

--- 1-FindEvenString.playground/Contents.swift
+++ 1-FindEvenString.playground/Contents.swift
@@ -2,22 +2,25 @@
 
 import UIKit
 
-// Find two even words in string
+// Find two even words in a string
 
-var myString: String = "To be or not to be ?"
+var myString: String = "To be, or not to be?"
+
 
 func findWords(newString: String) -> [String] {
+    let lowercaseString = newString.lowercased()
     var evenWords: [String] = []
-    // var word: Bool = true
-    let myArr: [String] = newString.components(separatedBy: " ") // ["To", "be", "or", "not", "to", "be", "?"]
+    
+    let myArr: [String] = lowercaseString.components(separatedBy: CharacterSet.init(charactersIn: ",? "))// ["to", "be", ",", "or", "not", "to", "be", "?"]
+    myArr.d
     for (index, _) in myArr.enumerated() {
         for i in index+1..<myArr.count {
             if myArr[index] == myArr[i] {
-            evenWords.append(myArr[index])
+                evenWords.append(myArr[index])
             }
         }
     }
-    print(evenWords) // ["be"]
+    print(evenWords) // ["to", "be"]
     return evenWords
 }
 
